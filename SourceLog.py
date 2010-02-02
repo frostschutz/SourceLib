35d05a6d883c06341d9c83d92ebfb7d60bebc5db
Andreas Klauer <Andreas.Klauer@metamorpher.de>
2010-02-02 17:43:58 +0100

    restored SourceLog.py from backup andreas.20100202.121009

---
+++ SourceLog.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+#------------------------------------------------------------------------------
+# SourceLog - Python class for parsing logs of Source Dedicated Servers
+# Copyright (c) 2010 Andreas Klauer <Andreas.Klauer@metamorpher.de>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#------------------------------------------------------------------------------
+
+import socket, asyncore
+
+class SourceLog(asyncore.dispatcher):
+    def __init__(self, host, port=27015, myhost='127.0.0.1', myport=17015):
+        asyncore.dispatcher.__init__(self)
+        self.create_socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.bind((myhost, myport))
+        #self.connect((host, port))
+
+    def handle_connect(self):
+        print "handle_connect"
+        pass
+
+    def handle_close(self):
+        print "handle_close"
+        self.close()
+
+    def handle_read(self):
+        print "handle_read"
+        data = self.recv(4096)
+        print repr(data)
+
+    def writable(self):
+        print "writable"
+        return False
+
+    def handle_write(self):
+        print "handle_write"
+        pass
+
+server = SourceLog('tfgather.de', 27035, myhost='127.0.0.1', myport=17035)
+
+asyncore.loop()
