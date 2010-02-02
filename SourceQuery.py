61eced78e1c3bbff5540159312f95225c2d0b2d4
Andreas Klauer <Andreas.Klauer@metamorpher.de>
2010-02-02 17:43:56 +0100

    restored SourceQuery.py from backup andreas.20100122.031008

--- SourceQuery.py
+++ SourceQuery.py
@@ -24,13 +24,9 @@
 # THE SOFTWARE.
 #------------------------------------------------------------------------------
 
-import socket, struct, time
+"""http://developer.valvesoftware.com/wiki/Server_Queries"""
 
-PACKETSIZE = 1400
-MAXSPLITSIZE = 1248
-MINSPLITSIZE = 564
-WHOLE = struct.pack("<l", -1)
-SPLIT = struct.pack("<l", -2)
+import socket, struct, time
 
 A2A_PING = 'i'
 A2A_PING_REPLY = 'j'
@@ -38,7 +34,15 @@ A2A_PING_REPLY = 'j'
 class SourceQueryError(Exception): pass
 
 class SourceQuery(object):
-    """http://developer.valvesoftware.com/wiki/Server_Queries"""
+    packetsize = 1400
+    maxsplit = 1248
+    minsplit = 564
+    whole = -1
+    split = -2
+
+    a2a_ping = 0x69
+
+
 
     def __init__(self, host, port=27015, timeout=1.0):
         self.host = host
@@ -49,6 +53,45 @@ class SourceQuery(object):
         self.challenge = False
         self.sendid = 0
         self.recvid = 0
+
+    def _pack(self, types, values):
+        raw = ''
+
+        for t, v in zip(types, values):
+            if t == 'byte':
+                raw += struct.pack('<B', v)
+            elif t == 'short':
+                raw += struct.pack('<h', v)
+            elif t == 'long':
+                raw += struct.pack('<l', v)
+            elif t == 'float':
+                raw += struct.pack('<f', v)
+            elif t == 'string':
+                raw += v + '\x00'
+
+        return raw
+
+    def _unpack(self, types, raw):
+        values = []
+
+        for t in types:
+            if t == 'byte':
+                values += list(struct.unpack('<B', raw[:1]))
+                raw = raw[1:]
+            elif t == 'short':
+                values += list(struct.unpack('<h', raw[:2]))
+                raw = raw[2:]
+            elif t == 'long':
+                values += list(struct.unpack('<l', raw[:4]))
+                raw = raw[4:]
+            elif t == 'float':
+                values += list(struct.unpack('<f', raw[:4]))
+                raw = raw[4:]
+            elif t == 'string':
+                values += [raw[:raw.index('\x00')]]
+                raw = raw[raw.index('\x00')+1:]
+
+        return (values,raw)
 
     def connect(self):
         if not self.connected:
@@ -94,10 +137,7 @@ class SourceQuery(object):
             return chunk[len(WHOLE):]
 
     def communicate(self, message):
-        # In theory we should split the message here if it's too long.
-        # In practice we don't issue queries longer than 1400 bytes,
-        # so we can always send the whole message.
-        self.send(message, 16)
+        self.send(message)
 
         # However, the reply we receive may be split.
         chunk = self.socket.recv(1400)
@@ -117,5 +157,6 @@ class SourceQuery(object):
 
 server = SourceQuery('intermud.de')
 
-server.connect()
-server.info()
+print Byte("1")
+#print Byte(struct.pack("<B", 16))
+
